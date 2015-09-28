fe71f13fa6a7f132d17cd470e992658fcdf11c22
Charles McKinnis <charles@mckinn.is>
2015-09-28 13:57:36 -0500

    The output config is handled by a function
    and returned as a string so the output is more portable.

--- gather_file.py
+++ gather_file.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python2
 
-infile = open("nginx.conf", 'r')
+import re
+import glob
 
-for line in infile
-    print line
+def importfile(var_filename, var_keyword):
+
+    var_files = glob.iglob(var_filename)
+    var_combined = ""
+
+    for var_onefile in var_files:
+        #infile = open(var_filename, 'r')
+        infile = open(var_onefile, 'r')
+
+        for line in infile:
+            #print "%s" % line.rstrip()
+            #print "%s" % line.strip() # removes whitespace on left and right
+            var_result = re.match('\s*%s\s+(\S+);' % var_keyword, line.strip() )
+            #var_result = re.match('(include.*)', line.strip(), re.I | re.U )
+            if var_result:
+                var_combined += "#"+line+"\n"
+                #print "#include %s " % var_result.group(1)
+                var_combined += importfile(var_result.group(1),var_keyword)
+            else:
+                var_combined += line
+                #print line.rstrip()
+    return var_combined
+
+print importfile("etc/nginx/nginx.conf","include")
