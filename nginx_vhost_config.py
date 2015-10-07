7642f3fad0f138d5b59fa1791f0b255835b43fea
Charles McKinnis <charles@mckinn.is>
2015-10-07 18:44:24 -0500

    nginx configuration is now placed in dictionary and list data structures correctly. A change is partially complete to move the nginx config parsing to a function that will return the dictionary.

--- nginx_vhost_config.py
+++ nginx_vhost_config.py
@@ -17,7 +17,6 @@ stanza_count = 0
 server_start = 0
 
 def importfile(filename, keyword):
-
     files = glob.iglob(filename)
     combined = ""
 
@@ -48,54 +47,55 @@ wholeconfig = importfile(conffile,"include")
 # list structure
 # [ server stanza { line : { listen: [ ], server_name : [ ], root { location : path } } } ]
 
-linenum = 0
-nginx_stanzas = {} #AutoVivification()
-for line in wholeconfig.splitlines():
-    linenum += 1
-    # this doesn't do well if you open and close a stanza on the same line
-    if len(re.findall('{',line)) > 0 and len(re.findall('}',line)) > 0:
-        print "This script does not consistently support opening { and closing } stanzas on the same line."
-    stanza_count+=len(re.findall('{',line))
-    stanza_count-=len(re.findall('}',line))
-    result = re.match('^\s*server', line.strip() )
-    if result:
-        server_start = stanza_count
-        server_line = str(linenum)
-        if not server_line in nginx_stanzas:
-            nginx_stanzas[server_line] = { }
-    # are we in a server block, and not a child stanza of the server block? is so, look for keywords
-    # this is so we don't print the root directive for location as an example. That might be useful, but isn't implemented at this time.
-    if server_start == stanza_count:
-        # we are in a server block
-        #result = re.match('\s*(listen|server|root)', line.strip())
-        result = re.match('\s*(listen|server_name|root)\s*(.*)', line.strip("\s\t;"))
+def nginx_config(wholeconfig):
+    linenum = 0
+    nginx_stanzas = {} #AutoVivification()
+    for line in wholeconfig.splitlines():
+        linenum += 1
+        # this doesn't do well if you open and close a stanza on the same line
+        if len(re.findall('{',line)) > 0 and len(re.findall('}',line)) > 0:
+            print "This script does not consistently support opening { and closing } stanzas on the same line."
+        stanza_count+=len(re.findall('{',line))
+        stanza_count-=len(re.findall('}',line))
+        result = re.match('^\s*server', line.strip() )
         if result:
-            #print line.strip()
-            if result.group(1)=="listen":
-                if not result.group(1) in nginx_stanzas[server_line]:
-                    nginx_stanzas[server_line][result.group(1)] = []
-                nginx_stanzas[server_line][result.group(1)] += [result.group(2)]
-                #nginx_stanzas[server_line][result.group(1)].append(result.group(2)) # WORKS!
-                #print result.group(2)
-                print "listen %s" % result.group(2)
-            if result.group(1)=="server_name":
-                if not result.group(1) in nginx_stanzas[server_line]:
-                    nginx_stanzas[server_line][result.group(1)] = []
-                nginx_stanzas[server_line][result.group(1)] += result.group(2).split()
-                #nginx_stanzas[server_line][result.group(1)].append(result.group(2)) # WORKS!
-                # nothing here
-                print "server_name %s" % result.group(2)
-            if result.group(1)=="root":
-                if not result.group(1) in nginx_stanzas[server_line]:
-                    nginx_stanzas[server_line][result.group(1)] = []
-                nginx_stanzas[server_line][result.group(1)] += result.group(2).split()
-                #nginx_stanzas[server_line][result.group(1)].append(result.group(2)) # WORKS!
-                # nothing here
-                print "root %s" % result.group(2)
-    # if the server block is bigger than the current stanza, we have left the server stanza we were in
-    #if server_start > stanza_count and server_start > 0: # The lowest stanza_count goes is 0, so it is redundant
-    if server_start > stanza_count:
-        # we are no longer in the server { block
-        server_start = 0
-        print ""
-print "%r" % nginx_stanzas
+            server_start = stanza_count
+            server_line = str(linenum)
+            if not server_line in nginx_stanzas:
+                nginx_stanzas[server_line] = { }
+        # are we in a server block, and not a child stanza of the server block? is so, look for keywords
+        # this is so we don't print the root directive for location as an example. That might be useful, but isn't implemented at this time.
+        if server_start == stanza_count:
+            # we are in a server block
+            #result = re.match('\s*(listen|server|root)', line.strip())
+            result = re.match('\s*(listen|server_name|root)\s*(.*)', line.strip("\s\t;"))
+            if result:
+                #print line.strip()
+                if result.group(1)=="listen":
+                    if not result.group(1) in nginx_stanzas[server_line]:
+                        nginx_stanzas[server_line][result.group(1)] = []
+                    nginx_stanzas[server_line][result.group(1)] += [result.group(2)]
+                    #nginx_stanzas[server_line][result.group(1)].append(result.group(2)) # WORKS!
+                    #print result.group(2)
+                    print "listen %s" % result.group(2)
+                if result.group(1)=="server_name":
+                    if not result.group(1) in nginx_stanzas[server_line]:
+                        nginx_stanzas[server_line][result.group(1)] = []
+                    nginx_stanzas[server_line][result.group(1)] += result.group(2).split()
+                    #nginx_stanzas[server_line][result.group(1)].append(result.group(2)) # WORKS!
+                    # nothing here
+                    print "server_name %s" % result.group(2)
+                if result.group(1)=="root":
+                    if not result.group(1) in nginx_stanzas[server_line]:
+                        nginx_stanzas[server_line][result.group(1)] = []
+                    nginx_stanzas[server_line][result.group(1)] += result.group(2).split()
+                    #nginx_stanzas[server_line][result.group(1)].append(result.group(2)) # WORKS!
+                    # nothing here
+                    print "root %s" % result.group(2)
+        # if the server block is bigger than the current stanza, we have left the server stanza we were in
+        #if server_start > stanza_count and server_start > 0: # The lowest stanza_count goes is 0, so it is redundant
+        if server_start > stanza_count:
+            # we are no longer in the server { block
+            server_start = 0
+            print ""
+    #print "%r" % nginx_stanzas
