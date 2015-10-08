82687ac19fd55ba10eaf7d67305e38c729371db8
Charles McKinnis <charles.mckinnis@rackspace.com>
2015-10-08 09:50:32 -0500

    parse_nginx_config is now fully a function.
    Removed print statements from the function, and it returns the dictionary of lists. It accepts a config file contents to parse. Although I'm using it with the whole config, you could pass it a single config too.

--- nginx_vhost_config.py
+++ nginx_vhost_config.py
@@ -13,8 +13,6 @@ class AutoVivification(dict):
             return value
 
 conffile = "./amalg.conf"
-stanza_count = 0
-server_start = 0
 
 def importfile(filename, keyword):
     files = glob.iglob(filename)
@@ -41,13 +39,15 @@ def importfile(filename, keyword):
 #infile = open(onefile, 'r')
 
 #wholeconfig = infile.readlines()
-wholeconfig = importfile(conffile,"include")
 #print wholeconfig
 
 # list structure
 # [ server stanza { line : { listen: [ ], server_name : [ ], root { location : path } } } ]
 
-def nginx_config(wholeconfig):
+def parse_nginx_config(wholeconfig):
+    stanza_count = 0
+    server_start = 0
+    location_start = 0
     linenum = 0
     nginx_stanzas = {} #AutoVivification()
     for line in wholeconfig.splitlines():
@@ -75,27 +75,26 @@ def nginx_config(wholeconfig):
                     if not result.group(1) in nginx_stanzas[server_line]:
                         nginx_stanzas[server_line][result.group(1)] = []
                     nginx_stanzas[server_line][result.group(1)] += [result.group(2)]
-                    #nginx_stanzas[server_line][result.group(1)].append(result.group(2)) # WORKS!
-                    #print result.group(2)
-                    print "listen %s" % result.group(2)
+                    #print "listen %s" % result.group(2)
                 if result.group(1)=="server_name":
                     if not result.group(1) in nginx_stanzas[server_line]:
                         nginx_stanzas[server_line][result.group(1)] = []
                     nginx_stanzas[server_line][result.group(1)] += result.group(2).split()
-                    #nginx_stanzas[server_line][result.group(1)].append(result.group(2)) # WORKS!
-                    # nothing here
-                    print "server_name %s" % result.group(2)
+                    #print "server_name %s" % result.group(2)
                 if result.group(1)=="root":
                     if not result.group(1) in nginx_stanzas[server_line]:
                         nginx_stanzas[server_line][result.group(1)] = []
                     nginx_stanzas[server_line][result.group(1)] += result.group(2).split()
-                    #nginx_stanzas[server_line][result.group(1)].append(result.group(2)) # WORKS!
-                    # nothing here
-                    print "root %s" % result.group(2)
+                    #print "root %s" % result.group(2)
         # if the server block is bigger than the current stanza, we have left the server stanza we were in
-        #if server_start > stanza_count and server_start > 0: # The lowest stanza_count goes is 0, so it is redundant
+        # if server_start > stanza_count and server_start > 0: # The lowest stanza_count goes is 0, so it is redundant
         if server_start > stanza_count:
             # we are no longer in the server { block
             server_start = 0
-            print ""
-    #print "%r" % nginx_stanzas
+            #print ""
+    return nginx_stanzas
+
+wholeconfig = importfile(conffile,"include")
+nginx_stanzas = parse_nginx_config(wholeconfig)
+print "%r" % nginx_stanzas
+
