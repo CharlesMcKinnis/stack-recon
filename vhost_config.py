ed002d20a8b7a9050987d082f20af72d01c402af
Charles McKinnis <charles.mckinnis@rackspace.com>
2015-10-26 16:28:53 -0500

    implementing memory profiling

--- vhost_config.py
+++ vhost_config.py
@@ -991,7 +991,8 @@ if "sites" in  globalconfig["apache"]:
         proc_mem = result
         proc_avg = proc_mem["line_sum"]/proc_mem["line_count"]
         proc_a = (proc_mem["line_sum"] / proc_mem["line_count"])
-        proc_avg_total = int( proc_a * proc_max)
+        #proc_avg_total = int( proc_a * proc_max)
+        proc_avg_total = proc_a * proc_max
         proc_max_total = proc_mem["biggest"]*proc_max
         proc_eighty = int( (result["line_sum"]+result["free_mem"]) / result["biggest"] * .8 )
         #memory_print(result, proc_name, proc_max)
