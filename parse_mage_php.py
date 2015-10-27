4259b8552f5c4a9f7143618829d369fa53d5b736
Charles McKinnis <charles.mckinnis@rackspace.com>
2015-10-27 16:54:35 -0500

    Adding a MagentoCtl class, and version parser function

--- parse_mage_php.py
+++ parse_mage_php.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 import re
-
+import os
 docroot = "/data/sites/purlsoho.com/live"
 onefile = os.path.join(docroot, "app", "Mage.php")
 onefile = "Mage.php"
@@ -22,15 +22,30 @@ grep Mage.php
         );
     }
 """
+class MagentoCtl(object):
+    def version(self, mage_php_file):
+        mage = {}
+        file_handle = open(mage_php_file, 'r')
+        for line in file_handle:
+            result = re.match("static\s+private\s+\$_currentEdition\s*=\s*self::([^\s;]+);", line.strip(), re.IGNORECASE )
+            if result:
+                mage["edition"] = result.group(1)
+            #result = re.match("public static function getVersionInfo\(\)", line.strip(), re.IGNORECASE)
+            if "public static function getVersionInfo()" in line:
+                line = file_handle.next() # {
+                line = file_handle.next() # return array(
+                while not ");" in line:
+                    line = file_handle.next()
+                    result = re.match("'([^']+)'\s*=>\s*'([^']*)'", line.strip())
+                    if result:
+                        mage[result.group(1)] = result.group(2)
+                break
+        file_handle.close()
+        # join them with periods, unless they are empty, then omit them
+        mage["version"] = ".".join(filter(None,[mage["major"],mage["minor"],mage["revision"],mage["patch"],mage["stability"],mage["number"]]))
+        return(mage)
 
-onefile_handle = open(onefile, 'r')
-for line in onefile_handle:
-    result = re.match("static\s+private\s+$_currentEdition\s*=\s*self::([^\s;]+);", line.strip(), re.IGNORECASE )
-    if result:
-        print result.group(1)
-    #result = re.match("public static function getVersionInfo\(\)", line.strip(), re.IGNORECASE)
-    if "public static function getVersionInfo()" in line:
-        print "yep, %s" % line
-
-        
+magento = MagentoCtl()
+mage = magento.version("Mage.php")
+print "Magento %s %s" % (mage["version"],mage["edition"])
 
--- vhost_config.py
+++ vhost_config.py
@@ -598,6 +598,29 @@ class phpfpmCtl(object):
                     stanzas["maxclients"] += int(stanzas[one]["pm.max_children"])
         return(stanzas)
 
+class MagentoCtl(object):
+    def version(self, mage_php_file):
+        mage = {}
+        file_handle = open(mage_php_file, 'r')
+        for line in file_handle:
+            result = re.match("static\s+private\s+\$_currentEdition\s*=\s*self::([^\s;]+);", line.strip(), re.IGNORECASE )
+            if result:
+                mage["edition"] = result.group(1)
+            #result = re.match("public static function getVersionInfo\(\)", line.strip(), re.IGNORECASE)
+            if "public static function getVersionInfo()" in line:
+                line = file_handle.next() # {
+                line = file_handle.next() # return array(
+                while not ");" in line:
+                    line = file_handle.next()
+                    result = re.match("'([^']+)'\s*=>\s*'([^']*)'", line.strip())
+                    if result:
+                        mage[result.group(1)] = result.group(2)
+                break
+        file_handle.close()
+        # join them with periods, unless they are empty, then omit them
+        mage["version"] = ".".join(filter(None,[mage["major"],mage["minor"],mage["revision"],mage["patch"],mage["stability"],mage["number"]]))
+        return(mage)
+
 def daemon_exe(match_exe):
     """
     var_filter = "text to search with"
