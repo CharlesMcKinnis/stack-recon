fee5f3b9f1ffd742cb19beddaa88e96d13d56642
Charles McKinnis <charles.mckinnis@rackspace.com>
2015-10-30 23:34:25 -0500

    working on xml parsing

--- test_parsing_xml.py
+++ test_parsing_xml.py
@@ -4,8 +4,10 @@ import xml.etree.ElementTree as ET
     
 tree = ET.ElementTree(file='local.xml')
 
-#root = tree.getroot()
+root = tree.getroot()
 #print root.tag, root.attrib
-for elem in tree.iter():
-    print elem.tag, elem.attrib
-    
+# .iter doesn't exist in python 2.6 on RHEL 6
+#for elem in tree.iter():
+#    print elem.tag, elem.attrib
+for child_of_root in root:
+    print child_of_root.tag, child_of_root.attr
