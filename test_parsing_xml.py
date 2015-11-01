737a7844d1418be268cc8547e4b4e3d50a4fab6f
Charles McKinnis <charles.mckinnis@rackspace.com>
2015-10-31 22:38:25 -0500

    Working xml extraction of db information

--- test_parsing_xml.py
+++ test_parsing_xml.py
@@ -1,16 +1,106 @@
 #!/usr/bin/env python2
 
+import sys
 import xml.etree.ElementTree as ET
 
 tree = ET.ElementTree(file='local.xml')
 
 def unroll(tree):
     for elem in tree.getiterator():
-        print "%r %r" % (elem.tag, elem.attrib)
-        #print type(elem)
+        print "9 %r %r" % (elem.tag, elem.attrib)
         for child in elem.getchildren():
-            print child.tag
+            print "11 %r" % child.tag
+            print "12 %r" % child.attrib
+            print "13 %r" % child.text
 root = tree.getroot()
+for children in root:
+    print "17 %s %s" % (children.tag,children.text)
+    for child in children:
+        print "19 %s %s" % (child.tag,child.text)
+    
+
+resources = tree.find('global/resources')
+#resources = root.find('config')
+print resources
+print "Table prefix: %s" % resources.find('db/table_prefix').text
+for i in resources.find('default_setup/connection'):
+    print "%s: %s" % (i.tag,i.text)
+    pass
+
+"""
+config/global/resources/
+    db/table_prefix     text
+    default_setup/connection
+        host     text
+        username     text
+        password     text
+        dbname     text
+        initStatements     text
+        model     text
+        type     text
+        active     text
+        persistent     text
+"""
+"""
+# redit sessions
+config/global
+    session_save     text
+    redis_session
+        host     text
+        port     text
+        password     text
+        timeout     text
+        persistent     text
+        db     text
+        compression_threshold     text
+        compression_lib     text
+        log_level     text
+        max_concurrency     text
+        break_after_frontend     text
+        break_after_adminthml     text
+        bot_lifetime     text
+"""
+"""
+#redis objects
+config/global
+    cache
+        backend     text
+        backend_options
+            server     text
+            port     text
+            persistent
+            database
+            password
+            force_standalone
+            connect_retries
+            read_timeout
+            automatice_cleaning_factor
+            compress_data
+            compress_tags
+            compress_threshold
+            compression_lib
+"""
+"""
+#redis FPC
+config/global
+    full_page_cache
+        backend
+        slow_backend
+        backend_options
+            server
+            port
+            persistent
+            database
+            force_standalone
+            connect_retries
+            read_timeout
+            automatice_cleaning_factor
+            compress_data
+            compress_tags
+            compress_threshold
+            compression_lib
+            
+"""
 #print root.tag, root.attrib
 # .iter doesn't exist in python 2.6 for RHEL 6
 #for elem in tree.getiterator('config'):
@@ -18,4 +108,4 @@ root = tree.getroot()
 #for child_of_root in root:
 #    print child_of_root.tag, child_of_root.attrib
 #print tree
-unroll(tree)
+#unroll(tree)
