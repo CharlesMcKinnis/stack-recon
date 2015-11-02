8fd70f3fec7fd88f85e21e456007d38ab0f028f1
Charles McKinnis <charles.mckinnis@rackspace.com>
2015-11-02 16:44:34 -0600

    Working on Magento data gathering

--- ecommStackStatus.py
+++ ecommStackStatus.py
@@ -658,6 +658,7 @@ class MagentoCtl(object):
                 lowest_i = 255
                 shortest = ""
                 for i in mage_php_matches:
+                    print "i 661: %r" % i
                     counter = re.findall('/',i)
                     if counter < lowest_i:
                         shortest = i
