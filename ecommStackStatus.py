bac5ce0eb57ac5482af123c1d1e801bb188f9fd2
Charles McKinnis <charles.mckinnis@rackspace.com>
2015-11-22 02:08:46 +0000

    Added a note to check cm_redissession.xml for Active set to True

--- ecommStackStatus.py
+++ ecommStackStatus.py
@@ -36,6 +36,10 @@ innodb_buffer_pool_size
 innodb_buffer_pool_instances
 innodb_log_buffer_size
 uery_cache_limit
+
+* also need to check, if session cache is using redis
+app/etc/modules/Cm_RedisSessions.xml
+value of <active> to true
 """
 STACK_STATUS_VERSION = 2015111202
 error_collection = []
