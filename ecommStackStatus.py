e56e524842dd4ead907691deefa303f4bbd738f4
Charles McKinnis <charles.mckinnis@rackspace.com>
2016-01-20 14:37:57 -0600

    Improving apache multiline VirtualHost parsing

--- ecommStackStatus.py
+++ ecommStackStatus.py
@@ -286,11 +286,11 @@ class apacheCtl(object):
             linecomp = line.strip().lower()
             # if the line opens < but doesn't close it with > there is probably a \ and newline
             # and it should be concat with the next line until it closes with >
-            if linecomp[-1].endswith('\\'):
+            if linecomp[-1].endswith("\\"):
                 #<" in linecomp and not ">" in linecomp:
                 # removing the trailing \
                 #linecomp = linecomp.strip("\\").strip()
-                while linecomp[-1].endswith('\\'):
+                while linecomp[-1].endswith("\\"):
                     linecomp = linecomp.strip("\\").strip()
                     # read the next line
                     line = lines.next()
