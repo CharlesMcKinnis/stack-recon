1c46d60c4c23d2b7a49663732f6b2b42096c576b
Charles McKinnis <charles.mckinnis@rackspace.com>
2016-11-22 11:13:44 -0600

    Promoting dev branch, fixing an oops from last commit

--- stack-recon/stackreconlib.py
+++ stack-recon/stackreconlib.py
@@ -61,11 +61,7 @@ cd stack-recon && git checkout -b dev origin/dev
 To look at the json captured:
 cat config_dump.json |python -m json.tool|less
 """
-<<<<<<< HEAD
-STACK_LIB_VERSION = 2016111601
-=======
 STACK_LIB_VERSION = 2016112201
->>>>>>> dev
 error_collection = []
 
 
