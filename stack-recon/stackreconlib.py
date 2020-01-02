546b72a34b7485f5cdc999eb75d0f7aec5c241c2
Charles McKinnis <charles.mckinnis@palmettostatearory.com>
2020-01-02 10:39:03 -0500

    bugfix except with a comma

--- stack-recon/stackreconlib.py
+++ stack-recon/stackreconlib.py
@@ -2074,7 +2074,7 @@ UnboundLocalError: local variable 'cursor' referenced before assignment
             try:
                 cnx = mysql.connector.connect(**config)
                 cursor = cnx.cursor()
-            except mysql.connector.Error, err:
+            except mysql.connector.Error as err:
                 if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
                     frameinfo = getframeinfo(currentframe())
                     print("Something is wrong with your user name or password, "
